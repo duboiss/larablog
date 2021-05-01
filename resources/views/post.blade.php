6c78a91e7bf617daae7d239969fa9271fb4a90a6
Steven DUBOIS <git@duboiss.fr>
2021-05-01 23:13:56 +0200

    Add blade layout component

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="/app.css">
-
-    <title>Laravel</title>
-</head>
-<body>
+<x-layout>
     <article>
-        <h1><?= $post->title ?></h1>
+        <h1>{{ $post->title }}</h1>
 
         <div>
-            <?= $post->body ?>
+            {!! $post->body !!}
         </div>
     </article>
 
     <a href="/">Go Back</a>
-</body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="/app.css">
-
-    <title>Laravel</title>
-</head>
-<body>
-    <?php foreach ($posts as $post): ?>
+<x-layout>
+    @foreach ($posts as $post)
         <h1>
-            <a href="/posts/<?= $post->slug ?>">
-                <?= $post->title ?>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
             </a>
         </h1>
 
-        <div><?= $post->excerpt ?></div>
-    <?php endforeach; ?>
-</body>
-</html>
+        <div>{{ $post->excerpt }}</div>
+    @endforeach
+</x-layout>
